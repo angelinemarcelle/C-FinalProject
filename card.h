b9b557582243f1ed9c6cb1cafd9165281f1e7b04
ELVISLUIHKU <125120914+ELVISLUIHKU@users.noreply.github.com>
2023-04-14 17:06:02 +0800

    Update card.h

--- card.h
+++ card.h
@@ -3,6 +3,9 @@ using namespace std;
 
 class Card {
 public:
+    static const int HEARTS = 0, DIAMONDS = 1, CLUBS = 2, SPADES = 3;
+    static const int TWO = 2, THREE = 3, FOUR = 4, FIVE = 5, SIX = 6, SEVEN = 7, EIGHT = 8, NINE = 9, TEN = 10;
+    static const int JACK = 11, QUEEN = 12, KING = 13, ACE = 14;
     Card(int suit, int rank); //constructor
     int get_suit() const; //getter
     int get_rank() const; //getter
