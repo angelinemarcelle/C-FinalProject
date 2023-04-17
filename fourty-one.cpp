dfea92c3be096316cbe42ff73d62f8c78f47a03f
Clarence Marvin <124359735+clarencemarvin@users.noreply.github.com>
2023-04-17 20:05:25 +0800

    Update fourty-one.cpp

--- fourty-one.cpp
+++ fourty-one.cpp
@@ -109,3 +109,114 @@ void takeCard(vector<Card> &hand, vector<Card> &deck) {
     return;
   }
 
+cout << "Choose a card index to discard (0-3): ";
+  cin >> discardIdx;
+
+  Card discardedCard = hand[discardIdx];
+  hand[discardIdx] = newCard;
+  // Remove the card from the deck as well
+  deck.push_back(discardedCard);
+
+  cout << "Discarded card: ";
+  printHand(vector<Card>{discardedCard});
+
+  sort(hand.begin(), hand.end(), [](Card a, Card b) {
+    int aValue =
+        a.face == "A"
+            ? 14
+            : (a.face == "J"
+                   ? 11
+                   : (a.face == "Q" ? 12
+                                    : (a.face == "K" ? 13 : stoi(a.face))));
+    int bValue =
+        b.face == "A"
+            ? 14
+            : (b.face == "J"
+                   ? 11
+                   : (b.face == "Q" ? 12
+                                    : (b.face == "K" ? 13 : stoi(b.face))));
+    return aValue < bValue;
+  });
+
+  cout << "Updated hand: ";
+  printHand(hand);
+
+  cout << endl << endl;
+}
+
+bool gameOver(vector<vector<Card>> &hands, vector<Card> &deck) {
+  for (auto hand : hands) {
+    if (getHandValue(hand) == 41)
+      return true;
+  }
+  return deck.empty();
+}
+
+int getWinner(vector<vector<Card>> &hands) {
+  int winnerIdx = -1;
+  int highest = 0;
+  for (int i = 0; i < hands.size(); i++) {
+    int score = getHandValue(hands[i]);
+    if (score > highest && score <= 41) {
+      highest = score;
+      winnerIdx = i;
+    }
+  }
+  return winnerIdx;
+}
+
+int main() {
+  srand(time(nullptr));
+
+  int numPlayers;
+  do {
+    cout << "How many players? (4-6): ";
+    cin >> numPlayers;
+    if (numPlayers < 4 || numPlayers > 6) {
+      cout << "Invalid number of players! Please input a number between 4-6."
+           << endl;
+    }
+  } while (numPlayers < 4 || numPlayers > 6);
+
+  vector<Card> deck;
+  for (int s = 0; s < 4; s++) {
+    for (int v = 2; v <= 14; v++) {
+      Card c;
+      if (v == 11)
+        c.face = "J";
+      else if (v == 12)
+        c.face = "Q";
+      else if (v == 13)
+        c.face = "K";
+      else if (v == 14)
+        c.face = "A";
+      else
+        c.face = to_string(v);
+      if (s == 0)
+        c.suit = "H";
+      else if (s == 1)
+        c.suit = "D";
+      else if (s == 2)
+        c.suit = "S";
+      else
+        c.suit = "C";
+      deck.push_back(c);
+    }
+  }
+
+  vector<vector<Card>> hands(numPlayers);
+  for (auto &hand : hands) {
+    deal(hand, deck, 4);
+  }
+
+  int currentPlayer = 0;
+  while (!gameOver(hands, deck)) {
+    cout << "Player " << currentPlayer + 1 << "'s turn:" << endl;
+    takeCard(hands[currentPlayer], deck);
+    currentPlayer = (currentPlayer + 1) % numPlayers;
+  }
+  int winner = getWinner(hands);
+  cout << "Player " << winner + 1 << " wins!" << endl;
+
+  return 0;
+}
