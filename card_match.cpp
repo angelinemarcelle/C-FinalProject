c97baea086296aea7111f02cc5bee84b26802fa4
Chanwoo <81244433+chanthr@users.noreply.github.com>
2023-05-01 15:45:24 +0800

    Update card_match.cpp

--- card_match.cpp
+++ card_match.cpp
@@ -1,49 +1,50 @@
 #include 'cardmatch.h'
 using namespace std;
 
-class Board {
+class Board { // updated class 30th April 2023
 public:
     int board_size;
     bool first_turn = true;
     vector<vector<string>> board;
     vector<vector<bool>> revealed;
-
-    void play_game();
-    void display_board(bool show_all = false);
-    vector<vector<string>> generate_board(int board_size);
+    int play();
+    void print(bool show_all = false);
+    vector<vector<string>> createBoard(int board_size);
+    int calculateweight();
 };
 
-void Board::play_game() {
+int Board::play() { // moved this from main function to void play() for better organisation
     int matches_found = 0;
     int total_pairs = (board_size * board_size) / 2;
-    auto start_time = chrono::steady_clock::now();
+    auto start_time = chrono::steady_clock::now(); // start timing
+    bool gameplay = true;
 
-    while (true) {
+    while (gameplay) { // game loop 
         if (first_turn) {
-            display_board(true);
+            print(true);
             first_turn = false;
-            cout << "Memorise the board for 5 seconds." << endl;
+            cout << "Memorise the picture of the board for 5 seconds" << endl;
             cout << "Do not press any key while this screen is on!" << endl;
             this_thread::sleep_for(chrono::seconds(5));
             system("clear");
             cout << "From now on, your play time will be recorded!" << endl;
-            display_board();
+            print();
         } else {
-            display_board();
+            print();
         }
-
-        int cell1, cell2;
+        
+        int cell1, cell2; // input variables
         cout << "Choose two cells to reveal (1-" << board_size * board_size << "): ";
         cin >> cell1 >> cell2;
 
-        if (cin.fail()) {
-            cout << "Invalid input. Please try again.\n";
+        if (cin.fail()) { // Check if the user has chosen the same cell twice
+            cout << "You have inputted an invalid input\n";
             cin.clear();
             cin.ignore(numeric_limits<streamsize>::max(), '\n');
             continue;
         }
 
-        int row1 = (cell1 - 1) / board_size;
+        int row1 = (cell1 - 1) / board_size; // Convert cell numbers to board indices
         int col1 = (cell1 - 1) % board_size;
         int row2 = (cell2 - 1) / board_size;
         int col2 = (cell2 - 1) % board_size;
@@ -52,37 +53,40 @@ void Board::play_game() {
             cout << "You cannot choose the same cell twice. Try again.\n";
             continue;
         } else if (revealed[row1][col1] || revealed[row2][col2]) {
-            cout << "You have already chosen that cell previously. Try again.\n";
+            cout << "You have already chosen that cell previously\n";
             continue;
         }
 
-        revealed[row1][col1] = true;
+        revealed[row1][col1] = true; // Reveal the cells
         revealed[row2][col2] = true;
 
-        if (board[row1][col1] != board[row2][col2]) {
+        if (board[row1][col1] != board[row2][col2]) { // Check if the cells match
+            system("clear"); // added system("clear") so that the player has a clear screen
+			print(false);
+			cout << "No match. Try again.\n" << endl;
+			revealed[row1][col1] = false;
+			revealed[row2][col2] = false;
+		} 
+		else {
             system("clear");
-            display_board(false);
-            cout << "No match. Try again.\n" << endl;
-            revealed[row1][col1] = false;
-            revealed[row2][col2] = false;
-        } else {
-            system("clear");
-            display_board();
-            matches_found++;
-            cout << "Match found!\n";
-            if (matches_found == total_pairs) {
-                auto end_time = chrono::steady_clock::now();
-                auto duration = chrono::duration_cast<chrono::seconds>(end_time - start_time);
-                cout << "You won!\n";
+			print();
+			matches_found++;
+			cout << "Match found!\n";
+			if (matches_found == total_pairs) { // winning condition 
+                auto end_time = chrono::steady_clock::now(); // record end time
+                auto duration = chrono::duration_cast<chrono::seconds>(end_time - start_time); // calculate duration
+				cout << "You won!\n";
                 cout << "You won in " << duration.count() << " seconds!\n";
-                break;
-            }
+                int time = duration.count();
+                return time;
+                gameplay = false;
+                } 
         }
     }
 }
 
-void Board::display_board(bool show_all) {
-    cout << '\n';
+void Board::print(bool show_all) { // have fixed the alignment issue
+    cout << '\n'; // add this line
     int cell = 1;
     for (int i = 0; i < board.size(); ++i) {
         for (int j = 0; j < board[i].size(); ++j) {
@@ -97,8 +101,8 @@ void Board::display_board(bool show_all) {
     }
 }
 
-vector<vector<string>> Board::generate_board(int board_size) {
-    vector<string> list;
+vector<vector<string>> Board::createBoard(int board_size) {
+    vector<string> list; // board definition 
     if (board_size == 4) {
         list = {"1♠", "1♠", "2♥", "2♥", "3♦", "3♦", "4♣", "4♣", "5♠", "5♠", "6♥","6♥", "7♦", "7♦", "8♣", "8♣"};
     } else if (board_size == 6) {
@@ -106,11 +110,13 @@ vector<vector<string>> Board::generate_board(int board_size) {
     } else {
         cerr << "Invalid board size\n";
         exit(1);
-    }shuffle(list.begin(), list.end(), mt19937{random_device{}()});
+    }
 
-    vector<vector<string>> board(board_size, vector<string>(board_size));
+    shuffle(list.begin(), list.end(), mt19937{random_device{}()}); // shuffling the elements
 
-    int index = 0;
+    vector<vector<string>> board(board_size, vector<string>(board_size)); // vector settings
+
+    int index = 0; // Initializing the board
     for (int i = 0; i < board_size; ++i) {
         for (int j = 0; j < board_size; ++j) {
             board[i][j] = list[index++];
@@ -119,12 +125,29 @@ vector<vector<string>> Board::generate_board(int board_size) {
     return board;
 }
 
-int main() {
-    Board game;
-    cout << "Choose the size of the board (4 or 6): ";
-    cin >> game.board_size;
-    game.board = game.generate_board(game.board_size);
-    game.revealed.resize(game.board_size, vector<bool>(game.board_size));
-    game.play_game();
+int calculateweight(int time, int size) {
+    if (size == 4) {
+        if (time <= 30) {
+            return 2;
+        }
+    } else {
+        if (time <= 60) {
+            return 3;
+        } else if (time >= 60 && time <= 120) {
+            return 2;
+        } else {
+            return 1;
+        }
+    }
+    return 0;
 }
 
+int main() { // shorter main function
+    Board game; // define so that it can be accessed
+    cout << "Choose the size of the board (4 or 6): ";
+    cin >> game.board_size;
+    game.board = game.createBoard(game.board_size); // creating board 
+    game.revealed.resize(game.board_size, vector<bool>(game.board_size));
+    int time = game.play();
+    calculateweight(time, game.board_size);
+}
