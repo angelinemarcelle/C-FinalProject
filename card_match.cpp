6764ece22d57fa9d19d028a589961f950906d939
Chanwoo <81244433+chanthr@users.noreply.github.com>
2023-05-01 17:09:49 +0800

    Update card_match.cpp

--- card_match.cpp
+++ card_match.cpp
@@ -7,13 +7,13 @@ public:
     bool first_turn = true;
     vector<vector<string>> board;
     vector<vector<bool>> revealed;
-    int play();
+    void play();
     void print(bool show_all = false);
     vector<vector<string>> createBoard(int board_size);
-    int calculateweight();
+    int calculateweight(int time);
 };
 
-int Board::play() { // moved this from main function to void play() for better organisation
+void Board::play() { // moved this from main function to void play() for better organisation
     int matches_found = 0;
     int total_pairs = (board_size * board_size) / 2;
     auto start_time = chrono::steady_clock::now(); // start timing
@@ -62,25 +62,25 @@ int Board::play() { // moved this from main function to void play() for better o
 
         if (board[row1][col1] != board[row2][col2]) { // Check if the cells match
             system("clear"); // added system("clear") so that the player has a clear screen
-			print(false);
-			cout << "No match. Try again.\n" << endl;
-			revealed[row1][col1] = false;
-			revealed[row2][col2] = false;
+		print(false);
+		cout << "No match. Try again.\n" << endl;
+		revealed[row1][col1] = false;
+		revealed[row2][col2] = false;
 		} 
-		else {
+	else {
             system("clear");
-			print();
-			matches_found++;
-			cout << "Match found!\n";
-			if (matches_found == total_pairs) { // winning condition 
-                auto end_time = chrono::steady_clock::now(); // record end time
-                auto duration = chrono::duration_cast<chrono::seconds>(end_time - start_time); // calculate duration
-				cout << "You won!\n";
-                cout << "You won in " << duration.count() << " seconds!\n";
-                int time = duration.count();
-                return time;
-                gameplay = false;
-                } 
+	    print();
+	    matches_found++;
+	    cout << "Match found!\n";
+	    if (matches_found == total_pairs) { // winning condition 
+		auto end_time = chrono::steady_clock::now(); // record end time
+		auto duration = chrono::duration_cast<chrono::seconds>(end_time - start_time); // calculate duration
+		cout << "You won!\n";
+		cout << "You won in " << duration.count() << " seconds!\n";
+		int time = duration.count();
+		calculateweight(time);
+		gameplay = false;
+		}
         }
     }
 }
@@ -91,9 +91,9 @@ void Board::print(bool show_all) { // have fixed the alignment issue
     for (int i = 0; i < board.size(); ++i) {
         for (int j = 0; j < board[i].size(); ++j) {
             if (revealed[i][j] || show_all) {
-                cout << setw(5) << board[i][j] << " ";
+                cout << setw(4) << board[i][j] << " ";
             } else {
-                cout << setw(5) << cell << " ";
+                cout << setw(4) << cell << " ";
             }
             ++cell;
         }
@@ -104,9 +104,9 @@ void Board::print(bool show_all) { // have fixed the alignment issue
 vector<vector<string>> Board::createBoard(int board_size) {
     vector<string> list; // board definition 
     if (board_size == 4) {
-        list = {"1♠", "1♠", "2♥", "2♥", "3♦", "3♦", "4♣", "4♣", "5♠", "5♠", "6♥","6♥", "7♦", "7♦", "8♣", "8♣"};
+        list = {"A", "A", "B", "B", "C", "C", "D", "D", "E", "E", "F","F", "G", "G", "H", "H"};
     } else if (board_size == 6) {
-        list = {"1♠", "1♠", "2♥", "2♥", "3♦", "3♦", "4♣", "4♣", "5♠", "5♠", "6♥","6♥", "7♦", "7♦", "8♣", "8♣", "9♠", "9♠", "10♥", "10♥", "11♦","11♦","12♣", "12♣"," 13♠"," 13♠"," 14♥"," 14♥"," 15♦"," 15♦"," 16♦"," 16♦","17♥","17♥"," 18♥"," 18♥"};
+        list = {"A", "A", "B", "B", "C", "C", "D", "D", "E", "E", "F","F", "G", "G", "H", "H", "I", "I", "J", "J", "K","K","L", "L","M","M","N","N","O","O","P","P","Q","Q","R","R"};
     } else {
         cerr << "Invalid board size\n";
         exit(1);
@@ -125,10 +125,13 @@ vector<vector<string>> Board::createBoard(int board_size) {
     return board;
 }
 
-int calculateweight(int time, int size) {
-    if (size == 4) {
+int Board::calculateweight(int time) {
+    if (board.size() == 4) {
         if (time <= 30) {
             return 2;
+        }
+        else {
+            return 1;
         }
     } else {
         if (time <= 60) {
@@ -146,8 +149,8 @@ int main() { // shorter main function
     Board game; // define so that it can be accessed
     cout << "Choose the size of the board (4 or 6): ";
     cin >> game.board_size;
+    
     game.board = game.createBoard(game.board_size); // creating board 
     game.revealed.resize(game.board_size, vector<bool>(game.board_size));
-    int time = game.play();
-    calculateweight(time, game.board_size);
+    game.play();
 }
