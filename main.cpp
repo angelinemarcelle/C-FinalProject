1f34c452c120e0586f4c04cfa51ab80cd1af1fed
Angeline Marcelle Lukito <87764018+angelinemarcelle@users.noreply.github.com>
2023-05-02 17:57:01 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -217,16 +217,58 @@ int main() {
                 bool valid_choice = false;
                 while (!valid_choice) {
                     cout << "Which game rule do you want to see?" << endl;
-                    cout << "Single player (Card Match), Double player (Switch Card), Multiplayer (41): (1-3)" << endl;
+                    cout << "1. Single player (Card Match)\n2. Double player (Switch Card)\n3. Multiplayer (41): (1-3)" << endl;
                     cin >> choice;
                     if (choice == 1) {
                         // view game rules for single player (Card Match)
+                        cout << "Welcome to Card Match, a simple single player game of memorization! \nIn this game, you will be challenged to memorize cards within a limited time frame." << endl;
+                        cout << endl;
+                        cout << "To play, follow these simple steps:" << endl;
+                        cout << "\t1. Choose the board size by entering 4 or 6. The bigger the board size, the more cards will be in play." << endl;
+                        cout << "\t2. You will be given 5 seconds to memorize the card elements displayed on the board." << endl;
+                        cout << "\t3. Enter two numbers that you think are the same." << endl;
+                        cout << endl;
+                        cout << "If you can successfully complete the game within the allotted time, you will win a prize based on your performance." << endl;
+                        cout << "For the 4x4 board, if you finish in under 30 seconds, you will receive double your initial wager. \nIf you finish in over 30 seconds, you will receive your initial wager." << endl;
+                        cout << "For the 6x6 board, you can win even more! If you finish in under 1 minute, you will receive three times your initial wager. \nIf you finish in under 2 minutes but over 1 minute, you will receive double your initial wager. \nIf you take over 2 minutes, you will still receive your initial wager." << endl;
+                        cout << endl;
+                        cout << "Get your mind ready and play to win more money! Good luck and happy playing." << endl;
                         valid_choice = true;
                     } else if (choice == 2) {
                         // view game rules for double player (Switch Card)
+                        cout << "Welcome to the Switch Card Dual player game! \nIn this game, two players will compete to get the highest possible value from a combination of 5 cards." << endl;
+                        cout << endl;
+                        cout << "Here's how to play:" << endl;
+                        cout << "\t1. First, each player will be given 5 cards randomly chosen from a deck of 52 cards." << endl;
+                        cout << "\t2. Each player will be shown their collection of cards, but keep it hidden from your opponent. You will then have the option to either keep your cards or swap them with random cards generated from the deck." << endl;
+                        cout << "\t3. If you choose to swap your cards, you will be given the option to choose how many and which cards to swap (1-5)." << endl;
+                        cout << "\t4. After both players have dealt their cards, both hands are revealed to each other. The player with the higher value of cards will win." << endl;
+                        cout << endl;
+                        cout << "To calculate your score, use the following values:" << endl;
+                        cout << "\t - Number cards represent their own numbers." << endl;
+                        cout << "\t - Face cards (Jack, Queen, King) represent 10." << endl;
+                        cout << "\t - Ace represents 11." << endl;
+                        cout << endl;
+                        cout << "If you win, you will receive all the wager from the other player. Since it's a double or nothing game, you'll need luck on your side!" << endl;
+                        cout << "Good luck and have fun playing!" << endl;
                         valid_choice = true;
                     } else if (choice == 3) {
                         // view game rules for multiplayer (41)
+                        cout << "Welcome to the 41 Multiplayer game! \nIn this game, you will be playing alongside 3 other players. The ultimate goal of the game is to get a score of 41 from a combination of 4 cards." << endl;
+                        cout << endl;
+                        cout << "Here's how to play:" << endl;
+                        cout << "\t1. To start the game, press \"y\" and hit enter." << endl;
+                        cout << "\t2. Your cards will be displayed on the screen, along with your current score. You will have the option to either keep your current hand of cards or switch one of your cards with a new card to try and get a better score." << endl;
+                        cout << "\t3. If you want to switch a card, press \"y\" and then choose the index of the card you want to switch (0-3). If you decide to keep your cards, press \"n\"." << endl;
+                        cout << "\t4. All players will take turns until one player gets a score of 41." << endl;
+                        cout << endl;
+                        cout << "To calculate your score, use the following values:" << endl;
+                        cout << "\t - Number cards represent their own numbers." << endl;
+                        cout << "\t - Face cards (Jack, Queen, King) represent 10." << endl;
+                        cout << "\t - Ace represents 11." << endl;
+                        cout << endl;
+                        cout << "If you win, you will receive the combined wager from all players. Remember, the winner takes it all, so play and strategize well!" << endl;
+                        cout << "Good luck and have fun playing!" << endl;
                         valid_choice = true;
                     } else {
                         cout << "Please input a valid number to view its game rules!" << endl;
