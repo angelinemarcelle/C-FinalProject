10ac051ecbac34ab487d4ed18040275db2475f76
Clarence Marvin <124359735+clarencemarvin@users.noreply.github.com>
2023-05-04 00:38:35 +0800

    Update switchCard.cpp

--- switchCard.cpp
+++ switchCard.cpp
@@ -2,40 +2,6 @@
 
 using namespacestd;
 
-#include <algorithm>
-#include <ctime>
-#include <iostream>
-#include <vector>
-#include <random>
-
-using namespace std;
-
-class Switch {
-public:
-    Switch(int numPlayers, int numCards);
-    void play();
-
-private:
-    struct Card {
-        string face;
-        string suit;
-    };
-
-    vector<Card> createDeck();
-    void shuffleDeck();
-    void dealCards();
-    void printHand(const vector<Card>& hand);
-    int getCardValue(const Card& card);
-    void swapCards(vector<Card>& hand, int cardIndex);
-    int scoreHand(const vector<Card>& hand);
-
-    const int numPlayers;
-    const int numCards;
-    vector<Card> deck;
-    vector<vector<Card>> hands;
-};
-
-
 Switch::Switch(int numPlayers, int numCards)
     : numPlayers(numPlayers), numCards(numCards), deck(createDeck()) {
     shuffleDeck();
