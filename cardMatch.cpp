e766d42996da00b2af41db0eaa39fd9ba7e6957a
winstongarth <125184321+winstongarth@users.noreply.github.com>
2023-05-04 17:57:15 +0800

    Update cardMatch.cpp

--- cardMatch.cpp
+++ cardMatch.cpp
@@ -1,11 +1,12 @@
 #include "cardMatch.h"
 
-void Board::play() { // moved this from main function to void play() for better organisation
+int Board::play() { // moved this from main function to void play() for better organisation
     int matches_found = 0;
     int total_pairs = (board_size * board_size) / 2;
     auto start_time = chrono::steady_clock::now(); // start timing
     bool gameplay = true;
     bool first_turn = true;
+    int time;
 
     while (gameplay) { // game loop 
         if (first_turn) {
@@ -22,14 +23,9 @@ void Board::play() { // moved this from main function to void play() for better 
         }
         
         int cell1, cell2; // input variables
-	cout << "You can type 0 to exit the game. However, your wager will be lost." << endl;
         cout << "Choose two cells to reveal (1-" << board_size * board_size << "): ";
         cin >> cell1 >> cell2;
-	    
-	if (cell1 == 0) {
-	    ::exit(0);
-	}
-
+    
         if (cin.fail()) { // Check if the user has chosen the same cell twice
             cout << "You have inputted an invalid input\n";
             cin.clear();
@@ -55,27 +51,26 @@ void Board::play() { // moved this from main function to void play() for better 
 
         if (board[row1][col1] != board[row2][col2]) { // Check if the cells match
             system("clear"); // added system("clear") so that the player has a clear screen
-		print(false);
-		cout << "No match. Try again.\n" << endl;
-		revealed[row1][col1] = false;
-		revealed[row2][col2] = false;
-		} 
-	else {
+            print(false);
+            cout << "No match. Try again.\n" << endl;
+            revealed[row1][col1] = false;
+            revealed[row2][col2] = false;
+        } 
+        else {
             system("clear");
-	    print();
-	    matches_found++;
-	    cout << "Match found!\n";
-	    if (matches_found == total_pairs) { // winning condition 
-		auto end_time = chrono::steady_clock::now(); // record end time
-		auto duration = chrono::duration_cast<chrono::seconds>(end_time - start_time); // calculate duration
-		cout << "You won!\n";
-		cout << "You won in " << duration.count() << " seconds!\n";
-		int time = duration.count();
-		calculateweight(time);
-		gameplay = false;
-		}
+            print();
+            matches_found++;
+            cout << "Match found!\n";
+            if (matches_found == total_pairs) { // winning condition 
+            auto end_time = chrono::steady_clock::now(); // record end time
+            auto duration = chrono::duration_cast<chrono::seconds>(end_time - start_time); // calculate duration
+            cout << "You finished the game in " << duration.count() << " seconds!\n";
+            time = duration.count();
+            gameplay = false;
+            }
         }
     }
+    return time;
 }
 
 void Board::print(bool show_all) { // have fixed the alignment issue
@@ -118,18 +113,18 @@ vector<vector<string>> Board::createBoard(int board_size) {
     return board;
 }
 
-int Board::calculateweight(int time) {
-    if (board.size() == 4) {
-        if (time <= 30) {
-            return 2;
+int Board::calculateweight(int time, int size) {
+    if (size == 4) {
+        if (time <= 120) {
+            return 3;
         }
         else {
             return 1;
         }
     } else {
-        if (time <= 60) {
+        if (time <= 180) {
             return 3;
-        } else if (time >= 60 && time <= 120) {
+        } else if (time >= 180 && time <= 240) {
             return 2;
         } else {
             return 1;
@@ -142,9 +137,11 @@ int game1() { // shorter main function
     Board game; // define so that it can be accessed
     cout << "Choose the size of the board (4 or 6): ";
     cin >> game.board_size;
-    
+    int size = game.board_size;
     game.board = game.createBoard(game.board_size); // creating board 
     game.revealed.resize(game.board_size, vector<bool>(game.board_size));
-    game.play();
-    return 0;
+    int value = game.play();
+    int weight = game.calculateweight(value, size);
+    return weight;
 }
+
